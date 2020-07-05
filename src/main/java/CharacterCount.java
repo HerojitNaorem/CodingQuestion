d23ba68c707eef283e021db3f52084d72626ce14
Herojit Naorem <herojit7@gmail.com>
2020-07-05 17:40:43 +0530

    branch1 second change

--- src/main/java/CharacterCount.java
+++ src/main/java/CharacterCount.java
@@ -2,5 +2,7 @@ public class CharacterCount {
     public static void main(String[] args) {
         System.out.println("hello programe");
         System.out.println("hello programe branch 1");
+
+        System.out.println("hello programe branch 1 second");
     }
 }
